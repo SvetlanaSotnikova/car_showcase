69794357bbb68dc8a02ad4911cdddcf6ddfa0e63
SvetlanaSotnikova <sotnikovas337@gmail.com>
2026-02-13 21:57:20 +0200

    update tailwind version from 4 -> 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import { Hero } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="overflow-hidden">
-       
+       <Hero />
       </main>
-    </div>
   );
 }
