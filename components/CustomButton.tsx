e4cb5971802bbbbf87d219332d96e172135b93c5
SvetlanaSotnikova <sotnikovas337@gmail.com>
2026-02-15 21:57:03 +0200

    fixed problems

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Image from 'next/image'
+import { CustomButtonProps } from '@/types'
 
-const CustomButton = () => {
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
     return (
         <button
             disabled={false}
