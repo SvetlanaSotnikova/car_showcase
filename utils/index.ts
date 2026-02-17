8ebc6b502f5b403463ea1351a00c15c384e041b8
SvetlanaSotnikova <sotnikovas337@gmail.com>
2026-02-17 10:09:27 +0200

    add util

--- utils/index.ts
+++ utils/index.ts
@@ -49,3 +49,19 @@ export const sanitizeCarData = (car: CarProps) => ({
   transmission: car.transmission ?? "a",
   year: toNumberOrDefault(car.year, 2020),
 });
+
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
