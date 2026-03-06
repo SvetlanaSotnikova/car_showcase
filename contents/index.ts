9d42d6f59d65e2f2c7c4c92e9d1044bf6d33ec53
SvetlanaSotnikova <sotnikovas337@gmail.com>
2026-03-06 20:50:04 +0200

    updated years

--- contents/index.ts
+++ contents/index.ts
@@ -44,7 +44,6 @@ export const manufacturers = [
 
 export const yearsOfProduction = [
   { title: "Year", value: "" },
-  { title: "2016", value: "2016" },
   { title: "2017", value: "2017" },
   { title: "2018", value: "2018" },
   { title: "2019", value: "2019" },
@@ -53,7 +52,6 @@ export const yearsOfProduction = [
   { title: "2022", value: "2022" },
   { title: "2023", value: "2023" },
   { title: "2024", value: "2024" },
-  { title: "2025", value: "2025" },
 ];
 
 export const fuels = [
