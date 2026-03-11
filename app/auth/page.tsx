5c8c468c25ddb946edb205dc770d93ea24738bc7
SvetlanaSotnikova <sotnikovas337@gmail.com>
2026-03-11 18:50:39 +0200

    fixed auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 import { CustomButton } from "@/components";
 import { FirebaseError } from "firebase/app";
 import { useAuth } from "@/contents/AuthContext";
+import { isAdmin } from "@/utils";
 
 const provider = new GoogleAuthProvider();
 
@@ -35,11 +36,15 @@ export default function AuthPage() {
   // errrors processing
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const isAdmin = user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL;
+  const admin = isAdmin(user?.email);
 
   useEffect(() => {
     if (!authLoading && user) {
-      router.replace("/cars");
+      if (isAdmin(user.email)) {
+        router.replace("/admin");
+      } else {
+        router.replace("/cars");
+      }
     }
   }, [user, authLoading, router]);
 
@@ -87,7 +92,6 @@ export default function AuthPage() {
       } else {
         await signInWithEmailAndPassword(auth, email, password);
       }
-      router.push(isAdmin? "/admin" : "/cars");
     } catch (error) {
       const firebaseError = error as FirebaseError;
       setError(getFirebaseError(error));
@@ -100,7 +104,6 @@ export default function AuthPage() {
     try {
       setLoading(true);
       await signInWithPopup(auth, provider);
-      router.push(isAdmin? "/admin" : "/cars");
     } catch (error) {
       const firebaseError = error as FirebaseError;
       setError(getFirebaseError(error));
@@ -118,75 +121,77 @@ export default function AuthPage() {
   return (
     <section className="overflow-hidden">
       <div className="mt-12 padding-x padding-y max-width">
-      <div className="mx-auto max-w-md bg-white p-6 rounded-lg shadow-md space-y-4">
-        <h1 className="text-2xl font-bold text-center">
-          {isRegister ? "Register" : "Login"}
-        </h1>
-        {error && (
-          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded-md text-sm text-center">
-            {error}
-          </div>
-        )}
+        <div className="mx-auto max-w-md bg-white p-6 rounded-lg shadow-md space-y-4">
+          <h1 className="text-2xl font-bold text-center">
+            {isRegister ? "Register" : "Login"}
+          </h1>
+          {error && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded-md text-sm text-center">
+              {error}
+            </div>
+          )}
 
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleEmailAuth();
-          }}
-          noValidate
-        >
-          <div className="space-y-4">
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full border rounded-md p-2"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={loading}
-              onKeyDown={handleKeyDown}
-              autoComplete="email"
-            />
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEmailAuth();
+            }}
+            noValidate
+          >
+            <div className="space-y-4">
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full border rounded-md p-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                disabled={loading}
+                onKeyDown={handleKeyDown}
+                autoComplete="email"
+              />
 
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full border rounded-md p-2"
-              value={password}
-              onKeyDown={handleKeyDown}
-              onChange={(e) => setPassword(e.target.value)}
-              autoComplete={isRegister ? "new-password" : "current-password"}
-              disabled={loading}
-            />
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full border rounded-md p-2"
+                value={password}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete={isRegister ? "new-password" : "current-password"}
+                disabled={loading}
+              />
 
-            <CustomButton
-              title={isRegister ? "Register" : "Login"}
-              handleClick={handleEmailAuth}
-              isDisabled={loading}
-              containerStyles={
-                "w-full bg-primary-blue text-white py-2 rounded-3xl"
-              }
-            ></CustomButton>
-          </div>
-        </form>
+              <CustomButton
+                title={isRegister ? "Register" : "Login"}
+                handleClick={handleEmailAuth}
+                isDisabled={loading}
+                containerStyles={
+                  "w-full bg-primary-blue text-white py-2 rounded-3xl"
+                }
+              ></CustomButton>
+            </div>
+          </form>
 
-        <CustomButton
-          title="Continue with Google"
-          handleClick={handleGoogleLogin}
-          isDisabled={loading}
-          containerStyles={"w-full bg-black text-white py-2 rounded-3xl"}
-          rightIcon="/google.svg"
-        ></CustomButton>
+          <CustomButton
+            title="Continue with Google"
+            handleClick={handleGoogleLogin}
+            isDisabled={loading}
+            containerStyles={"w-full bg-black text-white py-2 rounded-3xl"}
+            rightIcon="/google.svg"
+          ></CustomButton>
 
-        <p
-          className="text-sm text-center cursor-pointer text-gray-500"
-          onClick={() => setIsRegister(!isRegister)}
-          role="button"
-          tabIndex={0}
-          onKeyDown={(e) => e.key === "Enter" && setIsRegister((prev) => !prev)}
-        >
-          {isRegister ? "Already have an account?" : "Don't have an account?"}
-        </p>
-      </div>
+          <p
+            className="text-sm text-center cursor-pointer text-gray-500"
+            onClick={() => setIsRegister(!isRegister)}
+            role="button"
+            tabIndex={0}
+            onKeyDown={(e) =>
+              e.key === "Enter" && setIsRegister((prev) => !prev)
+            }
+          >
+            {isRegister ? "Already have an account?" : "Don't have an account?"}
+          </p>
+        </div>
       </div>
     </section>
   );
